9561fc292ee5a59f3078eff547a6cb3542ba660f
FerdynandHub <ferdynandcc@gmail.com>
2026-01-13 09:19:38 +0700

    Update ModeSelection.jsx

--- src/components/ModeSelection.jsx
+++ src/components/ModeSelection.jsx
@@ -90,7 +90,7 @@ export default function ModeSelection({
             description={
               userRole !== roles.VIEWER
                 ? "Access Knowledge Base"
-                : "Access restricted"
+                : "Access: Editor and Admin"
             }
             onClick={
               userRole !== roles.VIEWER
