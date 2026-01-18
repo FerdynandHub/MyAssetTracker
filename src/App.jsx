815ec8b60857a58a510c5b8868974275f0decc48
FerdynandHub <ferdynandcc@gmail.com>
2026-01-18 07:19:52 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -184,7 +184,7 @@ const App = () => {
           </div>
 
           <h1 className="text-3xl font-bold text-gray-800 mb-1 text-center">
-            Portal AVM UPH 5.0
+            Portal AVM UPH 5.2
           </h1>
 
           <p className="text-xs text-gray-400 text-center mb-4">
