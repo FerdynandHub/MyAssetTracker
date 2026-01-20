f20577aa572e29731811d74626c5d02f5c8304bf
FerdynandHub <ferdynandcc@gmail.com>
2026-01-20 11:47:11 +0700

    added pengajuan. stable

--- src/App.jsx
+++ src/App.jsx
@@ -202,7 +202,7 @@ const App = () => {
           </div>
 
           <h1 className="text-3xl font-bold text-gray-800 mb-1 text-center">
-            Portal AVM UPH 6.0
+            Portal AVM UPH 6.5
           </h1>
 
           <p className="text-xs text-gray-400 text-center mb-4">
@@ -422,7 +422,7 @@ return (
 {userRole === ROLES.EDITOR && (
   <SidebarItem
     icon={<FileText className="w-5 h-5" />}
-    label="Permintaan Saya"
+    label="Pengajuan Saya"
     active={mode === 'myRequests'}
     onClick={() => {
       setMode('myRequests');
