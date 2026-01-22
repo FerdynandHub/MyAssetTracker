e075847203ccc0acd3dc577fd5aec32ba089096a
Ferdynand chandra <ferdynandcc@gmail.com>
2026-01-22 15:30:09 +0700

    official stable 6.7

--- src/App.jsx
+++ src/App.jsx
@@ -202,7 +202,7 @@ const App = () => {
           </div>
 
           <h1 className="text-3xl font-bold text-gray-800 mb-1 text-center">
-            Portal AVM UPH 6.6
+            Portal AVM UPH 6.7
           </h1>
 
           <p className="text-xs text-gray-400 text-center mb-4">
