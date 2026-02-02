8a2fc75a6e3a1a3a653e4482b93250974833224a
Ferdynand chandra <ferdynandcc@gmail.com>
2026-02-02 18:46:43 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -689,9 +689,6 @@ width: Math.random() * 2 + 1 + 'px',
     height: Math.random() * 2 + 1 + 'px',
     top: Math.random() * 100 + '%',
     left: Math.random() * 100 + '%',
-    animationDelay: Math.random() * 3 + 's',
-    animationDuration: Math.random() * 2 + 1 + 's',
-    animationIterationCount: 'infinite'
           }}
         />
       ))}
